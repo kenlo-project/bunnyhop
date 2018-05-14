96f2ffcd1e06e6a309dafe4b72ed28474a7648dd
jiantangma <johntangma@gmail.com>
2018-05-14 19:23:23 +0300

    push build directory

---
+++ build/lib/plugins/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Logging = require('./logging.plugin');
+const Correlator = require('./correlator.plugin');
+const Package = require('./package.plugin');
+const Timeout = require('./timeout.plugin');
+
+module.exports = {
+  Logging,
+  Correlator,
+  Package,
+  Timeout
+};
+//# sourceMappingURL=index.js.map
